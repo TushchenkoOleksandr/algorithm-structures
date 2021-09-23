9b50d3ae66c950b2b30e111c4fde333d48fe4152
Oleksandr Tyshchenko <o.tyshchenko@spd-ukraine.com>
2021-09-23 10:44:49 +0300

    add poisk v shirinu

---
+++ 6_poisk_v_shirinu.js
@@ -0,0 +1,26 @@
+const graph = {
+  a: ["b", "c"],
+  b: ["f"],
+  c: ["d", "e"],
+  d: ["f"],
+  e: ["f"],
+  f: ["g"],
+};
+
+const breadthSearch = (graph, start, end) => {
+    let queue = [];
+    queue.push(start)
+    while(queue.length > 0) {
+        const current = queue.shift();
+        if(!graph[current]) {
+            graph[current] = []
+        }
+        if (graph[current].includes(end)) {
+            return true
+        } else {
+            queue = [...queue, ...graph[current]]
+        }
+    }
+}
+
+console.log(breadthSearch(graph, 'a', 'g'))
