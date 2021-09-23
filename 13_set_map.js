8e16ac522f3b4d92031168566e8f52dfc07e6a98
Oleksandr Tyshchenko <o.tyshchenko@spd-ukraine.com>
2021-09-23 12:44:47 +0300

    add map set

---
+++ 13_set_map.js
@@ -0,0 +1,18 @@
+const map = new Map();
+const objKey = { a: 2 };
+
+map.set(objKey, "test");
+
+console.log(map.get(objKey));
+
+const set = new Set();
+
+set.add(3);
+set.add(3);
+set.add(3);
+set.add(3);
+set.add(3);
+set.add(3);
+set.add(5);
+
+console.log(set);
